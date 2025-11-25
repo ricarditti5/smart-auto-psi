4c1b0692333494aac0b8beb3993de17a160217d2
Ricardo <ricarditti5@gmail.com>
2025-11-25 11:37:58 +0000

    First commit

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+# Documentação passo-a-passo do Projeto Smart-Auto
+- deve ter 1 classe base;
+- 2-3 classes derivadas(devem herdar da classe base, sobre-escrever um dos metodos, ter diferentes comportamentos);
+- uma das classes ser abstrata
+- implementar na iterface uma das classe
+- ter metodos comuns e usáveis
+- usar keyword base
+- usar polimorfismo
+ Classe base Veiculo
+ Classes derivadas: Carro, Mota, Camioneta
+ Polimorfismo: Acelerar(), Travão()
+ Interface: ISensor (ex.: detetar obstáculos)
+ Classe abstrata: VeiculoMotorizado
+ Uso do base nos construtores
+se possível: criar carro autónomo com comportamento especial.
+ */
+namespace Projeto_Smart_Auto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Veiculo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Smart_Auto
+{
+    //interface para as classes derivadas de veiculo
+    interface ISensor
+    {
+        void DetectarObstaculos();
+    }
+
+    abstract class VeiculoMotorizado
+    {
+        public abstract void EncherTanque();
+    }
+
+    //classe base veiculo
+    class Veiculo
+    {
+        public string placa { get; set; }
+        public string Acelerar(string carName)
+        {
+            return $"O Carro {carName} está a Acelerar";
+            //caso for fazer a simulação do carro usar:
+            //....
+            // posicaoCarro++; fazer com a diferença dos kilometros totais com o tempo
+        }
+        public string Travao(string carName)
+        {
+            //.....
+            //posicaoCarro = 0; pra fazer ele parar ou posicaoCarro --; por x tempo determindado
+            return $"O Carro {carName} está a Acelerar";
+        }
+    }
+}
